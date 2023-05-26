8c6c02b32dda091b31723faadfa94248e4176bfc
julianavalenti <julianabmartinez2@icloud.com>
2023-05-25 22:46:19 -0400

    adds fadeout effect

---
+++ assets/fadeout.js
@@ -0,0 +1,8 @@
+setTimeout(function() {
+    var imageContainer = document.getElementById('image-container');
+    imageContainer.style.display = 'none';
+  
+    var textContainer = document.getElementById('text-container');
+    textContainer.classList.add('show');
+  }, 3000);
+  
