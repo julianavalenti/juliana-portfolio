221ef9f9f4b3b28ce520d5d7d8daca4685737f5e
julianavalenti <julianabmartinez2@icloud.com>
2023-05-30 19:08:58 -0400

    adds contact form

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,20 +1,34 @@
 const contactFormHTML = `
-  <h2>Contact Form</h2>
-  <form id="contactForm">
-    <label for="name">Name:</label>
-    <input type="text" id="name" name="name" required><br>
-
-    <label for="phone">Phone:</label>
-    <input type="tel" id="phone" name="phone" required><br>
-
-    <label for="email">Email:</label>
-    <input type="email" id="email" name="email" required><br>
-
-    <label for="message">Message:</label><br>
-    <textarea id="message" name="message" rows="5" required></textarea><br>
-
-    <input type="submit" value="Submit">
-  </form>
+<div id="container">
+  <h1>&bull; Keep in Touch &bull;</h1>
+  <div class="underline">
+  </div>
+  <div class="icon_wrapper">
+    
+  </div>
+  <form action="https://formspree.io/f/mqkoovlb" method="post" id="contact_form">
+    <div class="name">
+      <label for="name"></label>
+      <input type="text" placeholder="My name is" name="name" id="name_input" required>
+    </div>
+    <div class="email">
+      <label for="email"></label>
+      <input type="email" placeholder="My e-mail is" name="email" id="email_input" required>
+    </div>
+    <div class="telephone">
+      <label for="name"></label>
+      <input type="text" placeholder="My number is" name="telephone" id="telephone_input" required>
+    </div>
+    
+    <div class="message">
+      <label for="message"></label>
+      <textarea name="message" placeholder="I'd like to chat about" id="message_input" cols="30" rows="5" required></textarea>
+    </div>
+    <div class="submit">
+      <input type="submit" value="Send Message" id="form_button" />
+    </div>
+  </form><!-- // End form -->
+</div><!-- // End #container -->
 `;
 
 
@@ -22,3 +36,5 @@ const formContainer = document.getElementById("formContainer");
 
 // Insert the contact form HTML into the container element
 formContainer.innerHTML = contactFormHTML;
+
+
