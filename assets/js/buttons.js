df11a716c13fc19fa5cfe03b7ed932ad023be9af
julianavalenti <julianabmartinez2@icloud.com>
2023-05-30 14:49:35 -0400

    adds first about me files

--- assets/js/buttons.js
+++ assets/js/buttons.js
@@ -1,13 +1,32 @@
 function loadPage1() {
-    document.getElementById('about-me-content').innerHTML = '<div id="text-container">' +
-        '<img class="about-me-avatar" src="assets/img/IMG_3416.JPG" alt="">' +
+    document.getElementById('about-me').innerHTML = '<div id="text-container">' +
+        
+    '<img class="about-me-avatar" src="assets/img/IMG_3416.JPG" alt="">' +
         '<div class="about-me-text">' +
         '<h2 class="about-me-h2">ABOUT ME</h2>' +
-        '<p class="about-me-p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt veniam at officia aspernatur minus enim, quae itaque accusantium deleniti nobis quidem, velit adipisci voluptatum sit rerum obcaecati pariatur ipsum aliquid?</p>' +
+
+        '<p class="about-me-p">I possess a commanding bachelors degree in psychology, with a strong foundation in neuroscience. Alongside my studies, I gained valuable experience as a student assistant, contributing to research projects and expanding my knowledge. Building on this academic success, I ventured into the field as a school psychologist, where I had the privilege of supporting and collaborating with over 360 students, as well as a dedicated team of 50+ teachers and staff.</p>' +
+        '<br>' +
+'<p class="about-me-p">When I moved to the United States, I made the bold decision to pursue my hobby as a profession: photography. Recognizing the power of visual storytelling and the ability to capture profound moments, I seized the opportunity and established my own photography business.</p>' +
+'<br>' +
+'<p class="about-me-p">However, my journey did not stop there. As I settled into my new home, I felt a deep yearning for personal growth and professional development. It was during this transformative phase that I discovered my fascination with software engineering—a field that intrigued me with its boundless possibilities and potential to impact lives.</p>' +
+'<br>'+
+'<p class="about-me-p">Motivated by my innate curiosity and driven by a thirst for knowledge, I delved into the world of software engineering. With each line of code I write, I embrace the opportunity to create innovative solutions and shape the digital landscape. My background as a psychologist empowers me to approach software engineering with a unique perspective, combining analytical thinking, empathy, and creativity.</p>' +
+'<br>'+
+'<p class="about-me-p">As I continue to expand my horizons in this ever-evolving field, I am dedicated to pushing the boundaries of what is possible, leveraging technology to make a positive impact on individuals and society as a whole. Through the fusion of psychology, photography, and software engineering, I aim to create meaningful experiences and drive transformative change.</p>' +
+
         '</div>' +
-        '</div>';
+        '</div>'+ '<div class="centered-buttons">' + '<button onclick="loadPage2()">Adventure Experience</button>';
 }
 
 function loadPage2() {
-    document.getElementById('about-me-content').innerHTML = '<p>This is Page 2 content.</p>';
+    document.getElementById('about-me').innerHTML = '<div id="text-container">' +
+    '<h1> Cultural Experience </h1>' +
+        
+    '<img class="about-me-avatar" src="assets/img/IMG_3491.JPG" alt="">' +'<img class="about-me-avatar" src="assets/img/IMG_3493.JPG" alt="">' +
+        '<div class="about-me-text">' +
+        '<p class="about-me-p">Since embarking on my journey as an immigrant and embracing the challenge of learning English from scratch, I have discovered a remarkable joy in adapting to new cultures. From a young age, I had the incredible opportunity to explore vibrant destinations with diverse cultures, immersing myself in the richness of different tribes, civilizations, customs, and histories. These awe-inspiring experiences have not only shaped my worldview but have also ignited a deep appreciation for the magic of multicultural environments.</p>'+
+        '<br>' +
+        '<p class="about-me-p"> Wholeheartedly, I champion the cause of multiculturalism, a philosophy that radiates warmth and inclusivity. It celebrates the myriad perspectives and unique contributions of individuals from all walks of life, fostering an atmosphere of mutual respect for our beautiful differences. Rather than imposing assimilation into a dominant culture, I firmly believe in the power of celebrating the kaleidoscope of voices that collectively form our society. In these spaces of acceptance, we cultivate boundless innovation, nurture collaborative spirit, and set the stage for a future where harmony and inclusiveness reign supreme.</p>';
+    
 }
